b4a995a0d550b74ff214219f40af5dbc222164ae
Aniket Singh <aniketsingh0104@gmail.com>
2021-03-14 18:13:39 +0530

    Add code for setup recycler view with notifications

--- app/src/main/java/com/dullabs/notiga/ui/appinbox/AppInboxFragment.kt
+++ app/src/main/java/com/dullabs/notiga/ui/appinbox/AppInboxFragment.kt
@@ -1,5 +1,7 @@
 package com.dullabs.notiga.ui.appinbox
 
+import android.annotation.SuppressLint
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -7,17 +9,22 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.navArgs
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.RecyclerView
 import com.dullabs.notiga.MainActivity
+import com.dullabs.notiga.R
+import com.dullabs.notiga.adapters.NotificationAdapter
 import com.dullabs.notiga.databinding.FragmentAppInboxBinding
-import com.dullabs.notiga.databinding.FragmentBatchBinding
+import com.dullabs.notiga.models.Notification
+import com.google.android.material.snackbar.Snackbar
 
 class AppInboxFragment : Fragment() {
 
     private var _binding: FragmentAppInboxBinding? = null
     private val appInboxViewModel: AppInboxViewModel by activityViewModels()
     private val appInboxFragmentArgs: AppInboxFragmentArgs by navArgs()
+    private lateinit var mNotificationAdapter: NotificationAdapter
 
     private val binding get() = _binding!!
 
@@ -28,17 +35,60 @@ class AppInboxFragment : Fragment() {
     ): View? {
         _binding = FragmentAppInboxBinding.inflate(inflater, container, false)
         appInboxViewModel.setAppName(appInboxFragmentArgs.appName)
-        appInboxViewModel.getAppName().observe(viewLifecycleOwner, Observer {
-            binding.textAppInbox.text = it
+        appInboxViewModel.init()
+        appInboxViewModel.getNotifications().observe(viewLifecycleOwner, Observer {
+            mNotificationAdapter.notifyDataSetChanged()
         })
         (activity as MainActivity).hideBottomControls()
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.appInboxHeaderText.text = appInboxFragmentArgs.appName
+        binding.appInboxHeaderIconImage.setImageResource(R.drawable.ic_whatsapp)
+        setupRecyclerView()
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         (activity as MainActivity).showBottomControls()
         _binding = null
+    }
+
+    private fun setupRecyclerView() {
+        mNotificationAdapter = NotificationAdapter(appInboxViewModel.getNotifications().value!!, requireContext()) {
+            // perform some action on notification clicked -> open the intended app
+        }
+        binding.appInboxRecyclerView.adapter = mNotificationAdapter
+        enableSwipeDeleteAndUndo()
+    }
+
+    private fun enableSwipeDeleteAndUndo() {
+        val swipeCallback: AppInboxSwipeCallback = object : AppInboxSwipeCallback(requireContext()) {
+            @SuppressLint("ShowToast")
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                val position: Int = viewHolder.adapterPosition
+                val notificationItem: Notification = mNotificationAdapter.getItemAtPosition(position)
+                appInboxViewModel.removeNotification(position)
+                val snackbar: Snackbar = Snackbar.make(
+                    (activity as MainActivity).getMainBinding().coordinatorLayout,
+                    "Item was removed from the list.",
+                    Snackbar.LENGTH_LONG
+                )
+                snackbar.setAction("Undo") {
+                    appInboxViewModel.restoreNotification(position, notificationItem)
+                    binding.appInboxRecyclerView.scrollToPosition(position)
+                }
+                snackbar.setActionTextColor(Color.YELLOW)
+                snackbar.setBackgroundTint(Color.DKGRAY)
+                snackbar.show()
+//                snackbar.setAnchorView(R.id.bottomFab).show()
+            }
+        }
+
+        val itemTouchHelper = ItemTouchHelper(swipeCallback)
+        itemTouchHelper.attachToRecyclerView(binding.appInboxRecyclerView)
     }
 
 }
--- app/src/main/java/com/dullabs/notiga/ui/appinbox/AppInboxViewModel.kt
+++ app/src/main/java/com/dullabs/notiga/ui/appinbox/AppInboxViewModel.kt
@@ -3,8 +3,43 @@ package com.dullabs.notiga.ui.appinbox
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.dullabs.notiga.models.Notification
+import com.dullabs.notiga.repositories.NotificationRepository
 
 class AppInboxViewModel : ViewModel() {
+
+    private lateinit var _mNotifications: MutableLiveData<ArrayList<Notification>>
+    private lateinit var mRepo: NotificationRepository
+
+    fun getNotifications(): LiveData<ArrayList<Notification>> {
+        return _mNotifications
+    }
+
+    fun init() {
+        if (this::_mNotifications.isInitialized) {
+            return
+        }
+        mRepo = NotificationRepository.getInstance()
+        _mNotifications = mRepo.getNotifications()
+    }
+
+    fun removeNotification(position: Int) {
+        // This part updates data base or api call
+//        mRepo.removeNotification(position)
+
+        val currentNotifications: ArrayList<Notification> = _mNotifications.value!!
+        currentNotifications.removeAt(position)
+        _mNotifications.postValue(currentNotifications)
+    }
+
+    fun restoreNotification(position: Int, notification: Notification) {
+        // This part updates data base or api call
+//        mRepo.restoreNotification(position, notification)
+
+        val currentNotifications: ArrayList<Notification> = _mNotifications.value!!
+        currentNotifications.add(position, notification)
+        _mNotifications.postValue(currentNotifications)
+    }
 
     private val _appName = MutableLiveData<String>()
 
